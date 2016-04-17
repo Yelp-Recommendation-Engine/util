e880d4264303208902cd7bd4270a97bf2d7c0245
tugceakin <tugce.eylul.akin@hotmail.com>
2016-04-17 13:20:59 -0700

    Import user, business and review dataset to Mongodb

--- insert_datasets_mongo.py
+++ insert_datasets_mongo.py
@@ -4,11 +4,23 @@ from pymongo import MongoClient
 
 connection = MongoClient("mongodb://localhost:27017")
 
-#connection = Connection('localhost', 27017) 
 db = connection.recommender 
-coll = db.businesses
-businesses = {}
+business_coll = db.businesses
+review_coll = db.reviews
+user_coll = db.users
 
 with open('yelp_academic_dataset_business.json', 'r') as f:
         for line in f:
-        	coll.insert(json.loads(line))
+        	business_coll.insert(json.loads(line))
+
+with open('yelp_academic_dataset_user.json', 'r') as f:
+        for line in f:
+        	user_coll.insert(json.loads(line))
+
+with open('yelp_academic_dataset_review.json', 'r') as f:
+        for line in f:
+	    	data = json.loads(line)
+	    	#Remove text field from each review
+	    	data.pop("text", None) 
+	        review_coll.insert(data)
+
